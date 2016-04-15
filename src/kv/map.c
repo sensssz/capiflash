cac8c8ac99a0718805c66092c84d541402fd2005
sensssz <hjmsens@gmail.com>
2016-04-15 13:10:33 -0400

    Bug fix in map_del checking.

--- src/kv/map.c
+++ src/kv/map.c
@@ -59,7 +59,8 @@ void map_del(map_t *map, uint8_t *key, uint64_t klen) {
   if (map->kvs[pos].key == NULL) {
     uint64_t index = 0;
     for (; index < map->cap; ++index) {
-      if (memcmp(map->kvs[pos].key, key, klen) == 0) {
+      if (map->kvs[index].key != NULL &&
+          memcmp(map->kvs[pos].key, key, klen) == 0) {
         printf("Key found at position %" PRId64 " while hashed to position %" PRId64 "\n", index, pos);
       }
     }
