7993a93016810967ca0670f55fdf55ffa8fb8a57
sensssz <hjmsens@gmail.com>
2016-04-16 16:52:03 -0400

    Bug fix.

--- src/kv/lru2l.c
+++ src/kv/lru2l.c
@@ -254,7 +254,7 @@ static void slist_access(slist_t *slist, uint8_t *key, uint64_t klen) {
 static void flist_validate(flist_t *lru) {
 #ifdef DEBUG
   if (lru->len != lru->hot_cache->size) {
-    printf("%" PRIu64 ", %" PRIu64 "\n", lru->len, lru->hot_cache);
+    printf("%" PRIu64 ", %" PRIu64 "\n", lru->len, lru->hot_cache->size);
   }
   if (lru->len == 0) {
     assert(lru->first == NULL && lru->last == NULL);
