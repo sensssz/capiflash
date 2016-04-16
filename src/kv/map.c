be71f26373f156a2784b1cb75bb9ed22ab50bf0b
sensssz <hjmsens@gmail.com>
2016-04-15 23:21:47 -0400

    Change output format.

--- src/kv/map.c
+++ src/kv/map.c
@@ -63,7 +63,7 @@ void map_del(map_t *map, uint8_t *key, uint64_t klen) {
     for (; index < map->cap; ++index) {
       if (map->kvs[index].key != NULL &&
           memcmp(map->kvs[index].key, key, klen) == 0) {
-        printf("Key found at position %" PRId64 " while hashed to position %" PRId64 "\n", index, pos);
+        printf("Key found at position %" PRIu64 " while hashed to position %" PRIu64 "\n", index, pos);
       }
     }
   }
@@ -167,9 +167,9 @@ static void print(map_t *map) {
   uint64_t  index = 0;
   for (; index < map->cap; ++index) {
     if (map->kvs[index].klen == 0) {
-      printf("%" PRId64 "\t----\t----\n", index);
+      printf("%" PRIu64 "\t----\t----\n", index);
     } else {
-      printf("%" PRId64 "\t%" PRId64 "\t%" PRId64 "\n", index, hash_hash(map->kvs[index].key, map->kvs[index].klen), map->kvs[index].off);
+      printf("%" PRIu64 "\t%" PRIu64 "\t%" PRIu64 "\n", index, map_pos(map, map->kvs[index].key, map->kvs[index].klen), map->kvs[index].off);
     }
   }
 }
