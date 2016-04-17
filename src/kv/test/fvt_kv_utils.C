7c5f6c0882fb10c7b926d1baa198358971e8a833
sensssz <hjmsens@gmail.com>
2016-04-16 23:12:00 -0400

    Remove asserts.

--- src/kv/test/fvt_kv_utils.C
+++ src/kv/test/fvt_kv_utils.C
@@ -30,7 +30,6 @@
  * \ingroup
  ******************************************************************************/
 #include <gtest/gtest.h>
-#include <ark.h>
 
 extern "C"
 {
@@ -214,7 +213,6 @@ void fvt_kv_utils_SGD_LOOP(ARK     *ark,
         puts("delete all key/value pairs from the db");
         /* delete all key/value pairs from the db */
         fvt_kv_utils_del(ark, db, LEN);
-        _ARK *_ark = (_ARK *) ark;
 
         puts("query all key/value pairs from the db");
         /* query all key/value pairs from the db */
